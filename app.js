849424c8296b58b059715ec3b780cd89d275d44e
JoshuaJHaines <joshjhaines3@gmail.com>
2022-03-07 17:04:21 -0500

    users are created, passwords hashed/salted, postman tests functioning

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const usersRouter = require("./routes/users/usersRouter");
 
 mongoose
 	.connect(process.env.MONGO_DB, {
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
